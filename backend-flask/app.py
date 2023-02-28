e45710fba63a12f00eb18f85275f9f224cd533d6
smbaig235 <smbaig251@gmail.com>
2023-02-28 01:53:05 +0000

    add simple span

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,7 +23,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, simple_processor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # Honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
