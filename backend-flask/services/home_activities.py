7119bf858dfe7091279ed41fb5624b0e656c399a
smbaig235 <smbaig251@gmail.com>
2023-03-17 20:24:44 +0000

    update home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -10,16 +10,14 @@ class HomeActivities:
   
   def run(cognito_user_id=None):
 
-     print("HOME ACTIVITY")
-
-   #logger.info("Home Activities")
+    #logger.info("Home Activities")
    #with tracer.start_as_current_span("home-activies-mock-data"):
     #span = trace.get_current_span()
     #now = datetime.now(timezone.utc).astimezone()
     #span.set_attribute("app.now", now.isoformat())
 
-     sql = query_wrap_array("""
-     SELECT
+      sql = query_wrap_array("""
+      SELECT
           activities.uuid,
           users.display_name,
           users.handle,
@@ -34,13 +32,13 @@ class HomeActivities:
         LEFT JOIN public.users ON users.uuid = activities.user_uuid
         ORDER BY activities.created_at DESC
         """)
-  print(sql)
-  with pool.connection() as conn:
-      with conn.cursor() as cur:
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
             cur.execute(sql)
             # this will return a tuple
             # the first field being the data
             json = cur.fetchone()
-  return json[0]
+      return json[0]
 
   
