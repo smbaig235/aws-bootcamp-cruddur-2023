593e68327b1377114edea938c85b6b93165db438
smbaig235 <smbaig251@gmail.com>
2023-03-17 20:41:07 +0000

    Test Query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -17,22 +17,8 @@ class HomeActivities:
     #span.set_attribute("app.now", now.isoformat())
 
       sql = query_wrap_array("""
-      SELECT
-          activities.uuid,
-          users.display_name,
-          users.handle,
-          activities.message,
-          activities.replies_count,
-          activities.reposts_count,
-          activities.likes_count,
-          activities.reply_to_activity_uuid,
-          activities.expires_at,
-          activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
+      SELECT * FROM activities
         """)
-      print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
             cur.execute(sql)
