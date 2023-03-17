0230a3b86636c9ece0d1a6522bd1579f9c876145
smbaig235 <smbaig251@gmail.com>
2023-03-17 00:35:49 +0000

    update home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -9,6 +9,7 @@ from lib.db import pool, query_wrap_object, query_wrap_array
 class HomeActivities:
   
   def run(cognito_user_id=None):
+
      print("HOME ACTIVITY")
 
    #logger.info("Home Activities")
@@ -17,29 +18,29 @@ class HomeActivities:
     #now = datetime.now(timezone.utc).astimezone()
     #span.set_attribute("app.now", now.isoformat())
 
-    sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
-    print(sql)
-    with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-    return json[0]
+     sql = query_wrap_array("""
+     SELECT
+          activities.uuid,
+          users.display_name,
+          users.handle,
+          activities.message,
+          activities.replies_count,
+          activities.reposts_count,
+          activities.likes_count,
+          activities.reply_to_activity_uuid,
+          activities.expires_at,
+          activities.created_at
+        FROM public.activities
+        LEFT JOIN public.users ON users.uuid = activities.user_uuid
+        ORDER BY activities.created_at DESC
+        """)
+  print(sql)
+  with pool.connection() as conn:
+    with conn.cursor() as cur:
+            cur.execute(sql)
+            # this will return a tuple
+            # the first field being the data
+            json = cur.fetchone()
+  return json[0]
 
- 
+  
