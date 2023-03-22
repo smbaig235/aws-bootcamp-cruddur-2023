8787fd939cd9c8ada67df9c97ab919d5030c00e8
smbaig235 <smbaig251@gmail.com>
2023-03-22 04:56:44 +0000

    update changes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,13 +33,13 @@ import logging
 from time import strftime
 
  #Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("test log")
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
 
 
 # X_Ray----
