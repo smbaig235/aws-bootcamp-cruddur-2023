c3e41b9ae41773ba442356778a2bb8c0d43d05ca
smbaig235 <smbaig251@gmail.com>
2023-03-22 04:47:38 +0000

    update db code for activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -8,10 +8,13 @@ from lib.db import db
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
+
     #with tracer.start_as_current_span("home-activites-mock-data"):
     #  span = trace.get_current_span()
     #  now = datetime.now(timezone.utc).astimezone()
     #  span.set_attribute("app.now", now.isoformat())
+
+
     sql = db.template('activities','home')
     results = db.query_array_json(sql)
     return results
