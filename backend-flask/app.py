aadef84ac6736466705c6de7d02072a80f82398f
smbaig235 <smbaig251@gmail.com>
2023-03-26 13:05:48 +0000

    update app file

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -212,7 +212,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle = 'sadaf'
+  user_handle = 'sadaf_23'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
