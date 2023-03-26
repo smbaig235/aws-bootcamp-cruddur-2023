04577463045ec58aa08048b3ee48f9f0cf25d036
smbaig235 <smbaig251@gmail.com>
2023-03-26 09:16:40 +0000

    update user and email

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Andrew Brown', 'andrewbrown' , 'abc123@mock.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
