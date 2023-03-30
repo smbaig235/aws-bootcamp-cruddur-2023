dda5693a668066aa35967b3955cf905c928e64b6
smbaig235 <smbaig251@gmail.com>
2023-03-30 08:01:51 +0000

    add authentication token in message form

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -2,6 +2,8 @@ import './MessageForm.css';
 import React from "react";
 import process from 'process';
 import { useParams } from 'react-router-dom';
+import checkAuth from '../lib/CheckAuth';
+
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -22,6 +24,7 @@ export default function ActivityForm(props) {
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
